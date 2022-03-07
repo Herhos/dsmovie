2faefff331c57e3421f7741e704e0c2fa22e4f31
Herhos <herhos@outlook.com>
2022-03-07 19:18:20 -0300

    Projeto criado

---
+++ backend/src/test/java/com/vix/dsmovie/DsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vix.dsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
