c97ba7a5a986221a307249af4eacfbe3c2ebf28a
Herhos <herhos@outlook.com>
2022-03-12 17:44:31 -0300

    Show score

--- frontend/src/components/MovieCards/index.tsx
+++ frontend/src/components/MovieCards/index.tsx
@@ -1,4 +1,3 @@
-import { type } from '@testing-library/user-event/dist/type';
 import MovieScores from 'components/MovieScores';
 import { Link } from 'react-router-dom';
 import { Movie } from 'types/movie';
@@ -15,7 +14,7 @@ function MovieCards( { movie } : Props)
          <div className="dsmovie-card-bottom-container">
             <h3>{movie.title}</h3>
 
-            <MovieScores />
+            <MovieScores count={movie.count} score={movie.score}/>
 
             <Link to={`/form/${movie.id}`}>
                <div className="btn btn-primary dsmovie-btn">Avaliar</div>
